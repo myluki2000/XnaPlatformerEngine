3c3f89504e9c71f94b8588387a7c4afb61b2757b
myluki2000 <lukas636@live.com>
2017-08-10 00:53:01 +0200

    Added cliffhanging

--- XnaPlatformerEngine/src/Characters/Collision.vb
+++ XnaPlatformerEngine/src/Characters/Collision.vb
@@ -27,43 +27,74 @@ Partial Public Class Character
     End Sub
 
     Private Function CheckCollidingVertical(displacement As Vector2) As Boolean
-        If displacement.Y < 0 Then
-            IsGrounded = False
-            Dim _rect = New Rectangle(getTrueRect.X, CInt(getTrueRect.Y + displacement.Y - 1), getTrueRect.Width, getTrueRect.Height)
-            For ind As Integer = 0 To CType(ScreenHandler.GetSelectedScreen, World).GetSelectedLevel.PlacedObjects.GetLength(0) - 1
-                Dim _wObj As WorldObject = CType(ScreenHandler.GetSelectedScreen, World).GetSelectedLevel.PlacedObjects(ind, CInt(Math.Floor(_rect.Top / 30)), 50) ' Z = 50 because z-index = 0 is 50 in the array
-                If _wObj IsNot Nothing Then
-                    If _rect.Intersects(_wObj.getTrueRect) Then
-                        Position.Y = _wObj.getTrueRect.Y + _wObj.getTrueRect.Height
-                        Acceleration.Y = 0
-                        Velocity.Y = 0
-                        Return True
+        Try
+            Dim SelectedLevel As Level = CType(ScreenHandler.GetSelectedScreen, World).GetSelectedLevel
+            Diagnostics.Debug.WriteLine(Velocity.Y)
+            If displacement.Y < 0 Then
+                IsGrounded = False
+                Dim _rect = New Rectangle(getTrueRect.X, CInt(getTrueRect.Y + displacement.Y - 1), getTrueRect.Width, getTrueRect.Height)
+                For ind As Integer = 0 To SelectedLevel.PlacedObjects.GetLength(0) - 1
+                    Dim _wObj As WorldObject = SelectedLevel.PlacedObjects(ind, CInt(Math.Floor(_rect.Top / 30)), 50) ' Z = 50 because z-index = 0 is 50 in the array
+                    If _wObj IsNot Nothing Then
+                        If _rect.Intersects(_wObj.getTrueRect) Then
+                            Position.Y = _wObj.getTrueRect.Y + _wObj.getTrueRect.Height
+                            Acceleration.Y = 0
+                            Velocity.Y = 0
+                            Return True
+                        End If
                     End If
-                End If
-            Next
-            Return False
+                Next
+                Return False
 
-        ElseIf displacement.Y > 0 Then
-            Dim _rect = New Rectangle(getTrueRect.X, CInt(getTrueRect.Y + displacement.Y + 1), getTrueRect.Width, getTrueRect.Height)
-            For ind As Integer = 0 To CType(ScreenHandler.GetSelectedScreen, World).GetSelectedLevel.PlacedObjects.GetLength(0) - 1
-                Dim _wObj As WorldObject = CType(ScreenHandler.GetSelectedScreen, World).GetSelectedLevel.PlacedObjects(ind, CInt(Math.Floor(_rect.Bottom / 30)), 50) ' Z = 50 because z-index = 0 is 50 in the array
-                If _wObj IsNot Nothing Then
-                    If _rect.Intersects(_wObj.getTrueRect) Then
-                        Position.Y = _wObj.getTrueRect.Y - getTrueRect.Height
-                        Acceleration.Y = 0
-                        Velocity.Y = 0
-                        IsGrounded = True
-                        Return True
+            ElseIf displacement.Y > 0 Then
+                Dim _rect = New Rectangle(getTrueRect.X, CInt(getTrueRect.Y + displacement.Y + 1), getTrueRect.Width, getTrueRect.Height)
+                For ind As Integer = 0 To SelectedLevel.PlacedObjects.GetLength(0) - 1
+                    Dim _wObj As WorldObject = SelectedLevel.PlacedObjects(ind, CInt(Math.Floor(_rect.Bottom / 30)), 50) ' Z = 50 because z-index = 0 is 50 in the array
+                    If _wObj IsNot Nothing Then
+                        If _rect.Intersects(_wObj.getTrueRect) Then
+                            Position.Y = _wObj.getTrueRect.Y - getTrueRect.Height
+                            Acceleration.Y = 0
+                            Velocity.Y = 0
+                            IsGrounded = True
+                            Return True
+                        End If
                     End If
-                End If
-            Next
+                Next
 
-            IsGrounded = False
-            Return False
+                Dim sideRectRight As New Rectangle(getTrueRect.Right, CInt(getTrueRect.Y), 10, 5)
+                For ind As Integer = 0 To SelectedLevel.PlacedObjects.GetLength(1) - 1
+                    Dim _wObj As WorldObject = SelectedLevel.PlacedObjects(CInt(Math.Floor(sideRectRight.Right / 30)), ind, 50)
+                    If _wObj IsNot Nothing Then
+                        If sideRectRight.Intersects(_wObj.getTrueRect) Then
+                            Acceleration.Y = 0
+                            Velocity.Y = 0
+                            IsGrounded = True
+                            Return True
+                        End If
+                    End If
+                Next
 
-        Else
-            Return False
-        End If
+                Dim sideRectLeft As New Rectangle(getTrueRect.Left - 10, CInt(getTrueRect.Y), 10, 5)
+                For ind As Integer = 0 To SelectedLevel.PlacedObjects.GetLength(1) - 1
+                    Dim _wObj As WorldObject = SelectedLevel.PlacedObjects(CInt(Math.Floor(sideRectLeft.Left / 30)), ind, 50)
+                    If _wObj IsNot Nothing Then
+                        If sideRectLeft.Intersects(_wObj.getTrueRect) Then
+                            Acceleration.Y = 0
+                            Velocity.Y = 0
+                            IsGrounded = True
+                            Return True
+                        End If
+                    End If
+                Next
+
+                IsGrounded = False
+                Return False
+
+            Else
+                Return False
+            End If
+        Catch ex As IndexOutOfRangeException
+        End Try
     End Function
 
     Private Function CheckCollidingSides(displacement As Vector2) As Boolean
