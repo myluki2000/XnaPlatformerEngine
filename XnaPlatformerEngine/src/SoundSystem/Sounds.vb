08aefecd386f5da5126813bacce8992264c93602
myluki2000 <lukas636@live.com>
2017-08-10 12:31:37 +0200

    Sounds structure

---
+++ XnaPlatformerEngine/src/SoundSystem/Sounds.vb
@@ -0,0 +1,25 @@
+﻿Public Class Sounds
+    Public Class World
+
+    End Class
+
+    Public Class Characters
+        Public Class Player
+
+        End Class
+    End Class
+
+    Public Class Music
+
+    End Class
+
+    Public Class Weapons
+        Public Class AR
+
+        End Class
+
+        Public Class Pistol
+
+        End Class
+    End Class
+End Class
