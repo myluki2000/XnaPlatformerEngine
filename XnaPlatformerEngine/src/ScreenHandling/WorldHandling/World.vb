c031064d1f4eaf2489155e4a3d83e04d9bbfd0ae
myluki2000 <lukas636@live.com>
2017-09-18 21:37:58 +0200

    Replaced get/set methods with property and removed old code

--- XnaPlatformerEngine/src/ScreenHandling/WorldHandling/World.vb
+++ XnaPlatformerEngine/src/ScreenHandling/WorldHandling/World.vb
@@ -7,40 +7,35 @@ Public Class World
     Inherits Screen
 
     Public Levels As New List(Of Level)
-    Private SelectedLevel As Level
+    Private _selectedLevel As Level
     Public Player As New Player With {.Scale = 2}
 
     Sub New()
         Matrix.CreateTranslation(0, 0, 0, LevelCameraMatrix)
     End Sub
 
+    Public Property SelectedLevel As Level
+        Get
+            Return _selectedLevel
+        End Get
+        Set(value As Level)
+            _selectedLevel = value
+
+            For x As Integer = 0 To SelectedLevel.PlacedObjects.GetLength(0) - 1
+                For y As Integer = 0 To SelectedLevel.PlacedObjects.GetLength(1) - 1
+                    If SelectedLevel.PlacedObjects(x, y, 50) IsNot Nothing AndAlso SelectedLevel.PlacedObjects(x, y, 50).GetType = GetType(PlayerSpawn) Then
+                        Player.Position = SelectedLevel.PlacedObjects(x, y, 50).getTrueRect.Location.ToVector2
+                    End If
+                Next
+            Next
+        End Set
+    End Property
+
     Public Sub LoadContent(Content As ContentManager)
         For Each _level In Levels
             _level.LoadContent(Content)
         Next
-
-        PostContentLoad()
     End Sub
-
-    Public Overrides Sub PostContentLoad()
-
-    End Sub
-
-    Public Sub SetSelectedLevel(index As Integer)
-        SelectedLevel = Levels(index)
-
-        For x As Integer = 0 To SelectedLevel.PlacedObjects.GetLength(0) - 1
-            For y As Integer = 0 To SelectedLevel.PlacedObjects.GetLength(1) - 1
-                If SelectedLevel.PlacedObjects(x, y, 50) IsNot Nothing AndAlso SelectedLevel.PlacedObjects(x, y, 50).GetType = GetType(PlayerSpawn) Then
-                    Player.Position = SelectedLevel.PlacedObjects(x, y, 50).getTrueRect.Location.ToVector2
-                End If
-            Next
-        Next
-    End Sub
-
-    Public Function GetSelectedLevel() As Level
-        Return SelectedLevel
-    End Function
 
     Public Overrides Sub Draw(sb As SpriteBatch)
 
