2f6bd95a773709a4e0fe231846d0e97b174058c5
myluki2000 <lukas636@live.com>
2018-12-15 17:04:03 +0100

    Fixed XmlSoundParser for new NoteSong

--- XnaPlatformerEngine/src/ResourceHandling/SoundSystem/XmlSound/XMLSoundParser.vb
+++ XnaPlatformerEngine/src/ResourceHandling/SoundSystem/XmlSound/XMLSoundParser.vb
@@ -6,18 +6,15 @@ Public Class XmlSoundParser
 
         Dim resultSong As New NoteSong
 
-        For Each trackEles In root.Elements("Track")
-            Dim tmpTrack As New Track
 
-            For Each noteEle In trackEles.Elements("n")
-                tmpTrack.Notes.Add(New Note() With {
+        For Each noteEle In root.Elements("n")
+            resultSong.Notes.Add(New Note() With {
                     .Duration = CInt(noteEle.Element("d")),
                     .Pitch = CInt(noteEle.Element("p"))
                 })
-            Next
-
-            resultSong.Tracks.Add(tmpTrack)
         Next
+
+
 
         Return resultSong
     End Function
